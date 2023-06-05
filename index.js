e9fa2fd2ca091e0cb1475aee37e49341aab31eb4
Abdurrasheed <abdurrasheedfalaludaura@gmail.com>
2023-06-05 20:32:06 +0100

    hover effect completed

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-let front = document.querySelector(".front-container");
+let front = document.querySelector(".front");
+let back = document.querySelector(".back");
 
+let btn = document.querySelector(".on");
+
+btn.addEventListener("click", () =>{
+    front.style.display = "none"
+    back.style.display = "block"
+})
