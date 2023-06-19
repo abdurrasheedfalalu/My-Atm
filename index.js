59892f194eb370b7e1cbc3bd64aa7d2eafe81f5b
Abdurrasheed <abdurrasheedfalaludaura@gmail.com>
2023-06-19 14:23:48 +0100

    deleted index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-let front = document.querySelector(".front");
-let back = document.querySelector(".back");
-
-let btn = document.querySelector(".on");
-
-btn.addEventListener("click", () =>{
-    front.style.display = "none"
-    back.style.display = "block"
-})
